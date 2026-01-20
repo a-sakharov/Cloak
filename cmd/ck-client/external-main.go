4e2ef402abb4990bd2910611f0a884adf4a2a3cf
Aleksandr Sakharov <sakharov-me@yandex-team.ru>
2026-01-20 21:15:07 +0300

    Fix tabs/spaces

--- cmd/ck-client/external-main.go
+++ cmd/ck-client/external-main.go
@@ -5,19 +5,19 @@ package main
 
 import (
 	"os"
-        "C"
-        "unsafe"
+	"C"
+	"unsafe"
 )
 
 //export external_main
 func external_main(argc C.int, argv **C.char) {
-        var offset = unsafe.Sizeof(uintptr(0))
-        var go_argv []string
-        for i := 0; i < int(argc); i++ {
-                go_argv = append(go_argv, C.GoString(*argv))
-                argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
-        }
+	var offset = unsafe.Sizeof(uintptr(0))
+	var go_argv []string
+	for i := 0; i < int(argc); i++ {
+		go_argv = append(go_argv, C.GoString(*argv))
+		argv = (**C.char)(unsafe.Pointer(uintptr(unsafe.Pointer(argv)) + offset))
+	}
 
-        os.Args = go_argv
-        main()
+	os.Args = go_argv
+	main()
 }
